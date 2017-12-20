80726f02b361a43f7f717c2bbb5077c5cffba1cf
Patrick Gaskill <patrick.gaskill@gmail.com>
2017-12-20 17:50:25 -0500

    Switch to react-virtualized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import React from "react";
+import { AutoSizer, Grid } from "react-virtualized";
+import "react-virtualized/styles.css";
 import "./App.css";
+import defaultPokedex from "./pokedex.json";
 
 class App extends React.Component {
+  state = {
+    pokedex: defaultPokedex
+  };
+
+  cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
+    const { pokedex } = this.state;
+    switch (columnIndex) {
+      case 0:
+        return (
+          <div key={key} style={style}>
+            {pokedex[rowIndex].id}
+          </div>
+        );
+      case 1:
+        return (
+          <div key={key} style={style}>
+            {pokedex[rowIndex].name}
+          </div>
+        );
+      case 2:
+        return (
+          <div key={key} style={style}>
+            {pokedex[rowIndex].seen ? "Yes" : "No"}
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
   render() {
-    return <div className="App" />;
+    return (
+      <AutoSizer>
+        {({ height, width }) => (
+          <Grid
+            cellRenderer={this.cellRenderer}
+            columnCount={3}
+            columnWidth={100}
+            height={height}
+            rowCount={this.state.pokedex.length}
+            rowHeight={30}
+            width={width}
+          />
+        )}
+      </AutoSizer>
+    );
   }
 }
 
