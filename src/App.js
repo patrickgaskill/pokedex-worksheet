515c16cd476475384f958fa89ac006947cadf862
Patrick Gaskill <patrick.gaskill@gmail.com>
2017-12-26 17:19:40 -0500

    Add seen column

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactTable from "react-table";
+import update from "immutability-helper";
 import "react-table/react-table.css";
 import "./App.css";
 import defaultPokedex from "./pokedex.json";
@@ -11,27 +12,37 @@ class App extends React.Component {
 
   formatPokemonNumber = id => `#${id.toString().padStart(3, "0")}`;
 
+  handleSeenClick = index => e => {
+    this.setState(prevState => ({
+      pokedex: update(prevState.pokedex, { [index]: { $toggle: ["seen"] } })
+    }));
+  };
+
+  columns = [
+    {
+      Header: "No.",
+      accessor: "id",
+      Cell: props => this.formatPokemonNumber(props.value)
+    },
+    { Header: "Name", accessor: "name" },
+    {
+      Header: "Seen",
+      accessor: "seen",
+      Cell: props => (
+        <input
+          type="checkbox"
+          checked={props.value}
+          onClick={this.handleSeenClick(props.index)}
+        />
+      ),
+      style: { textAlign: "center" }
+    }
+  ];
+
   render() {
     const { pokedex } = this.state;
-
-    const columns = [
-      {
-        Header: "No.",
-        accessor: "id",
-        Cell: row => this.formatPokemonNumber(row.value)
-      },
-      { Header: "Name", accessor: "name" },
-      { Header: "Seen", accessor: "seen" }
-    ];
-
     return (
-      <ReactTable
-        data={pokedex}
-        showPagination={false}
-        defaultPageSize={pokedex.length}
-        filterable
-        columns={columns}
-      />
+      <ReactTable className="-striped" data={pokedex} columns={this.columns} />
     );
   }
 }
