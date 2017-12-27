45c74365804240dc9120b76c43b402ebd26aa9a7
Patrick Gaskill <patrick.gaskill@gmail.com>
2017-12-26 23:38:12 -0500

    Add filters

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import update from "immutability-helper";
-import { Label } from "semantic-ui-react";
+import { Segment, Form, Label } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 import {
@@ -15,7 +15,17 @@ import defaultPokedex from "./pokedex.json";
 
 class App extends React.Component {
   state = {
+    pokedexFilter: "all",
+    includeSpecials: true,
     pokedex: defaultPokedex
+  };
+
+  handleIncludeSpecialsChange = (e, { checked }) => {
+    this.setState({ includeSpecials: checked });
+  };
+
+  handlePokedexFilterChange = (e, { value }) => {
+    this.setState({ pokedexFilter: value });
   };
 
   handleSeenClick = index => e => {
@@ -38,6 +48,23 @@ class App extends React.Component {
         [index]: { variants: { $toggle: [variant] } }
       })
     }));
+  };
+
+  getFilteredPokedex = () => {
+    const { pokedexFilter, includeSpecials, pokedex } = this.state;
+    let filtered = pokedex;
+
+    if (pokedexFilter === "uncaught") {
+      filtered = filtered.filter(p => !Object.values(p.genders).includes(true));
+    } else if (pokedexFilter === "genders") {
+      filtered = filtered.filter(p => Object.values(p.genders).includes(false));
+    }
+
+    if (!includeSpecials) {
+      filtered = filtered.filter(p => !p.regional && !p.legendary);
+    }
+
+    return filtered;
   };
 
   columns = [
@@ -96,24 +123,61 @@ class App extends React.Component {
   ];
 
   render() {
-    const { pokedex } = this.state;
+    const { pokedexFilter, includeSpecials } = this.state;
     return (
-      <ReactTable
-        className="-striped"
-        data={pokedex}
-        columns={this.columns}
-        getTrProps={(state, rowInfo) => {
-          if (rowInfo) {
-            if (rowInfo.original.legendary) {
-              return { style: { background: "#FFECB3" } };
-            } else if (rowInfo.original.regional) {
-              return { style: { background: "#C8E6C9" } };
+      <div>
+        <Segment basic>
+          <Form>
+            <Form.Group inline>
+              <Form.Radio
+                label="Show all"
+                name="pokedexFilterGroup"
+                value="all"
+                checked={pokedexFilter === "all"}
+                onChange={this.handlePokedexFilterChange}
+              />
+              <Form.Radio
+                label="Show uncaught"
+                name="pokedexFilterGroup"
+                value="uncaught"
+                checked={pokedexFilter === "uncaught"}
+                onChange={this.handlePokedexFilterChange}
+              />
+              <Form.Radio
+                radio
+                label="Show missing genders"
+                name="pokedexFilterGroup"
+                value="genders"
+                checked={pokedexFilter === "genders"}
+                onChange={this.handlePokedexFilterChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Checkbox
+                label="Include regionals and legendaries"
+                checked={includeSpecials}
+                onChange={this.handleIncludeSpecialsChange}
+              />
+            </Form.Group>
+          </Form>
+        </Segment>
+        <ReactTable
+          className="-striped"
+          data={this.getFilteredPokedex()}
+          columns={this.columns}
+          getTrProps={(state, rowInfo) => {
+            if (rowInfo) {
+              if (rowInfo.original.legendary) {
+                return { style: { background: "#FFECB3" } };
+              } else if (rowInfo.original.regional) {
+                return { style: { background: "#C8E6C9" } };
+              }
             }
-          }
 
-          return {};
-        }}
-      />
+            return {};
+          }}
+        />
+      </div>
     );
   }
 }
