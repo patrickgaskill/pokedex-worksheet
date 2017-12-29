b14e70c272bf3d9a93d2a1a341fb532d7d0c0a95
Patrick Gaskill <patrick.gaskill@gmail.com>
2017-12-29 00:53:29 -0500

    Remove why-did-you-update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { whyDidYouUpdate } from "why-did-you-update";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
-// if (process.env.NODE_ENV !== "production") {
-//   whyDidYouUpdate(React);
-// }
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
