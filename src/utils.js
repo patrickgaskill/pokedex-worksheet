d6b1bf09abe191fc91504bb6eb9c51f77b4a0a4f
Patrick Gaskill <patrick.gaskill@gmail.com>
2017-12-29 07:10:51 -0500

    Change lookup functions to use maps

--- src/utils.js
+++ src/utils.js
@@ -43,35 +43,79 @@ export const mapGenderToContent = (gender: Gender) => {
 };
 
 export const mapGenderToIcon = (gender: Gender) => {
-  switch (gender) {
-    case "MALE":
-    case "MALE_SHINY":
-      return "man";
-    case "FEMALE":
-    case "FEMALE_SHINY":
-      return "woman";
-    case "GENDERLESS":
-    case "GENDERLESS_SHINY":
-      return "genderless";
-    default:
-      return undefined;
-  }
+  const genderMap = {
+    MALE: "man",
+    MALE_SHINY: "man",
+    FEMALE: "woman",
+    FEMALE_SHINY: "woman",
+    GENDERLESS: "genderless",
+    GENDERLESS_SHINY: "genderless",
+    UNOWN_A: null,
+    UNOWN_B: null,
+    UNOWN_C: null,
+    UNOWN_D: null,
+    UNOWN_E: null,
+    UNOWN_F: null,
+    UNOWN_G: null,
+    UNOWN_H: null,
+    UNOWN_I: null,
+    UNOWN_J: null,
+    UNOWN_K: null,
+    UNOWN_L: null,
+    UNOWN_M: null,
+    UNOWN_N: null,
+    UNOWN_O: null,
+    UNOWN_P: null,
+    UNOWN_Q: null,
+    UNOWN_R: null,
+    UNOWN_S: null,
+    UNOWN_T: null,
+    UNOWN_U: null,
+    UNOWN_V: null,
+    UNOWN_W: null,
+    UNOWN_X: null,
+    UNOWN_Y: null,
+    UNOWN_Z: null
+  };
+  return genderMap[gender];
 };
 
 export const mapGenderToColor = (gender: Gender) => {
-  switch (gender) {
-    case "MALE":
-    case "MALE_SHINY":
-      return "blue";
-    case "FEMALE":
-    case "FEMALE_SHINY":
-      return "pink";
-    case "GENDERLESS":
-    case "GENDERLESS_SHINY":
-      return "grey";
-    default:
-      return "grey";
-  }
+  const genderMap = {
+    MALE: "blue",
+    MALE_SHINY: "blue",
+    FEMALE: "pink",
+    FEMALE_SHINY: "pink",
+    GENDERLESS: "grey",
+    GENDERLESS_SHINY: "grey",
+    UNOWN_A: "grey",
+    UNOWN_B: "grey",
+    UNOWN_C: "grey",
+    UNOWN_D: "grey",
+    UNOWN_E: "grey",
+    UNOWN_F: "grey",
+    UNOWN_G: "grey",
+    UNOWN_H: "grey",
+    UNOWN_I: "grey",
+    UNOWN_J: "grey",
+    UNOWN_K: "grey",
+    UNOWN_L: "grey",
+    UNOWN_M: "grey",
+    UNOWN_N: "grey",
+    UNOWN_O: "grey",
+    UNOWN_P: "grey",
+    UNOWN_Q: "grey",
+    UNOWN_R: "grey",
+    UNOWN_S: "grey",
+    UNOWN_T: "grey",
+    UNOWN_U: "grey",
+    UNOWN_V: "grey",
+    UNOWN_W: "grey",
+    UNOWN_X: "grey",
+    UNOWN_Y: "grey",
+    UNOWN_Z: "grey"
+  };
+  return genderMap[gender];
 };
 
 export const mapVariantToContent = (variant: Variant) => {
