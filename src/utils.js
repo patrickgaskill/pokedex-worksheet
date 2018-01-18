97486453e09c552739eb390afdb71651d048dc2a
Patrick Gaskill <patrick.gaskill@gmail.com>
2018-01-18 15:36:19 -0500

    Move formatPokemonNumber to utils

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,5 @@
+export const formatPokemonNumber = (number: number) =>
+  "#" + number.toString().padStart(3, "0");
+
 export const countTrueValues = (o: {}, callback?: mixed => boolean) =>
   Object.values(o).filter(el => (callback ? callback(el) : Boolean(el))).length;
